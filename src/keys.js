b332d3dd2e1fb916aaf041eb45b73bdab72b726f
helberadrian <helberadrian@gmail.com>
2022-05-26 21:28:53 -0300

    Entorno y Procesos
    1.0

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
     mongodb: {
         URI: 'mongodb://localhost:27017/mongo-database'
     }
 }
+
+const node = process.env.NODE;
+const host = process.env.HOST;
--- src/server.js
+++ src/server.js
@@ -5,17 +5,31 @@ const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const flash = require('connect-flash');
+const parseArgs = require('minimist');
 
 // Ini
 const app = express();
 require('./database');
 require('./passport/auth');
+require('./keys');
+
+// minimist
+const options = {
+    alias: {
+        p: 'puerto'
+    },
+    default: {
+        puerto: 8080
+    }
+}
+
+const command = process.argv.slice(2);
+const { puerto } = parseArgs(command, options);
 
 // settings
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan('dev'));
@@ -38,6 +52,6 @@ app.use((req, res, next) =>{
 app.use('/', require('./routes/index'));
 
 // starting server
-app.listen(app.get('port'), () => {
-    console.log('Servidor conectado escuchando en puerto ', app.set('port'));
+app.listen(puerto, process.env.HOST, () => {
+    console.log(`Servidor conectado escuchando en http://${process.env.HOST}:${puerto}`);
 });
